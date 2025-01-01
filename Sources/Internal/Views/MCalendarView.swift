ce166a8c1f10ed70606c6cd2982fb7a8168802da
Johann Burgess <40236773+yo-bur@users.noreply.github.com>
2025-01-01 22:40:00 +1100

    Update MCalendarView.swift

--- Sources/Internal/Views/MCalendarView.swift
+++ Sources/Internal/Views/MCalendarView.swift
@@ -43,7 +43,7 @@ private extension MCalendarView {
             .padding(.bottom, configData.monthsPadding.bottom)
             .background(configData.monthsViewBackground)
         }
-        .scrollTargetBehavior(.paging)
+        //.scrollTargetBehavior(.paging)
         .onAppear() { scrollToDate(reader, animatable: false) }
         .onChange(of: configData.scrollDate) { _ in scrollToDate(reader, animatable: true) }
     }}
